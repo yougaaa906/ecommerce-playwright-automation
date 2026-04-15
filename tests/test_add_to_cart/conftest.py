f5ef961cbaba84ec8d1fec91d6118f273340989b
yougaaa906 <yougaaa@163.com>
2026-04-16 00:35:21 +0800

    Update conftest.py

--- tests/test_add_to_cart/conftest.py
+++ tests/test_add_to_cart/conftest.py
@@ -2,8 +2,8 @@ import pytest
 from pages.product_detail_page import ProductDetailPage
 
 @pytest.fixture(scope="function", autouse=True)
-def clear_cart_after_test(page):
+def clear_cart_after_test(base_page):
     """Clear cart after each cart-related test to prevent data pollution"""
     yield
-    product_page = ProductDetailPage(page)
+    product_page = ProductDetailPage(base_page)
     product_page.clear_cart()
